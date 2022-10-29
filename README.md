f2d0b9ca036c4d18ec76a06db5d39a3fe1e7fcd4
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2022-10-29 11:06:15 +0100

    push stuf

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # EthLisbon-Hackathon
  
+Todolist:
+- Base implementation
+- Implement list of ERC20s to acc transfer
+- Implement whitelist addreses
+- Implement threshold value wrt time
+- Have a queue of txns
+- Have a password thingy / hash for panic instead of owner
+- Make it gassless
+
+
+- Put on Gnosis & Cronos
+- Implement w Safe or Metamask
--- wallet_saver_contracts/contracts/wallet_saver.sol
+++ wallet_saver_contracts/contracts/wallet_saver.sol
@@ -5,6 +5,7 @@ contract wallet_saver {
     bytes32 tx_content_hash;
     uint256 block_time_start;
     uint256 time_delay;
+    address panic_address;
 
     modifier _is_owner() {
         require(msg.sender == owner);
@@ -30,14 +31,28 @@ contract wallet_saver {
         uint256 _value,
         bytes memory _data
     ) public payable _is_owner {
-        // require()
+        require(
+            block.timestamp > block_time_start + time_delay,
+            "You can only execute after the time delay"
+        );
+        require(
+            keccak256(abi.encodePacked(_to, _value, _data)) == tx_content_hash
+        );
         (bool success, bytes memory result) = _to.call{value: _value}(_data);
         require(success, "something went wrong - tx failed");
     }
 
-    // function revert() {}
+    function revert_txn() public _is_owner {
+        block_time_start = 999999999999999999999999999999999999;
+        tx_content_hash = bytes32(0);
+    }
 
-    // function panic() {}
+    function panic() public _is_owner {
+
+        panic_address
+    }
+
+    
 
     // function change_owner(address new_owner) public _is_owner {
     //     owner = new_owner;

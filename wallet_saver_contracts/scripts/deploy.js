35885abd1140a5b80f9709a94620d254e5725cfa
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2022-10-30 00:45:54 +0100

    push

--- wallet_saver_contracts/scripts/deploy.js
+++ wallet_saver_contracts/scripts/deploy.js
@@ -3,7 +3,8 @@ const { ethers } = require("hardhat");
 async function main() {
 
     const wallet_saver_contract = await ethers.getContractFactory("wallet_saver_queue");
-    const wallet_saver_contract_acc = await wallet_saver_contract.deploy(100, "0x79e2514aac65d0359849b093c089756fc101b3be");
+    const wallet_saver_contract_acc = await wallet_saver_contract.deploy(100, "0x79e2514aac65d0359849b093c089756fc101b3be", "YOUR ADDRESS");
+    console.log(wallet_saver_contract_acc.owner())
 
     await wallet_saver_contract_acc.deployed();
 }
